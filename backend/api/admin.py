6ebd6c04c930cbf03d2fe0aac57f58aa54c12dd7
Facundo <facku995@gmail.com>
2023-05-12 22:41:49 -0300

    se agregaron la clase vuelo y asiento

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Usuario, Pago
+from .models import Usuario, Pago,Vuelo,Asiento
 # Register your models here.
 
 admin.site.register(Usuario)
 admin.site.register(Pago)
+admin.site.register(Vuelo)
+admin.site.register(Asiento)
