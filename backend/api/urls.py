641404fbc49fbb30fb063cecae0af8994c0a5057
Lucas-Balduzzi <lucasnicolasbalduzzi@gmail.com>
2023-05-14 22:44:01 -0300

    CarritoDEcompras 1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include
 from rest_framework import routers
-from api import views
+from apiview import views
 
 router=routers.DefaultRouter()
 router.register(r'usuarios',views.UsuarioViewSet)
 router.register(r'pagos',views.PagosViewSet)
+router.register(r'carritocompra',views.CarritoCompraViewSet)
 urlpatterns = [
     path('',include(router.urls))
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from .serializer import UsuarioSerializer, PagoSerializer
-from .models import Usuario, Pago
+from .serializer import UsuarioSerializer, PagoSerializer, CarritoCompraSerializer
+from .models import Usuario, Pago, CarritoCompra
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset=Usuario.objects.all()
@@ -13,3 +13,16 @@ class PagosViewSet(viewsets.ModelViewSet):
         usuario_id = self.request.data.get('usuario') # aquí obtenemos el id del usuario
         usuario = Usuario.objects.get(id=usuario_id) # y lo buscamos en la base de datos
         serializer.save(usuario=usuario) # pasamos el objeto encontrado al serializer
+
+
+
+
+
+
+class CarritoCompraViewSet(viewsets.ModelViewSet):
+    queryset= CarritoCompra.objects.all()
+    serializer_class=CarritoCompraSerializer
+    def perform_create(self, serializer):
+        usuario_id = self.request.data.get('usuario')
+        usuario = Usuario.objects.get(id=usuario_id)
+        serializer.save(usuario=usuario)
