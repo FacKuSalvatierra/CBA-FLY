ecec3dc37434ea2128ad13d64f74ea4dfc2d1f2d
Lucas-Balduzzi <lucasnicolasbalduzzi@gmail.com>
2023-05-14 23:03:00 -0300

    corregido api import views implementacion de template

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from rest_framework import routers
-from apiview import views
+from api import views
 
 router=routers.DefaultRouter()
 router.register(r'usuarios',views.UsuarioViewSet)
