eb8c8581d7d026bd481267267c7bdc918f9387b9
Lucas-Balduzzi <lucasnicolasbalduzzi@gmail.com>
2023-05-14 22:46:24 -0300

    carro compras 1

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Usuario, Pago
+from .models import Usuario, Pago, CarritoCompra
 # Register your models here.
 
 admin.site.register(Usuario)
 admin.site.register(Pago)
+admin.site.register(CarritoCompra)
