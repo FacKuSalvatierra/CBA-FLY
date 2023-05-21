fe0cf737a16e3cd78b7962bef992c7a261744106
Lucas-Balduzzi <lucasnicolasbalduzzi@gmail.com>
2023-05-20 22:26:51 -0300

    Borrado de contenido de rama por comando git rm <archivo> -r

--- backend/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Usuario, Pago,Vuelo,Asiento, CarritoCompra
-# Register your models here.
-
-admin.site.register(Usuario)
-admin.site.register(Pago)
-admin.site.register(Vuelo)
-admin.site.register(Asiento)
-admin.site.register(CarritoCompra)
