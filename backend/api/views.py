2ebee44ff7eef2c012f22692c4150c76703f83bb
RominaRodriguezMoreau <rominarodriguezmoreau@gmail.com>
2023-06-04 17:33:16 -1100

    terminando de diseñar views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,81 @@
 from rest_framework import viewsets
-from .serializer import UsuarioSerializer, PagoSerializer,VueloSerializer, AsientoSerializer,CarritoCompraSerializer
+from django.contrib.auth.models import User 
+from rest_framework import status , generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import UsuarioSerializer, PagoSerializer,VueloSerializer, AsientoSerializer, CarritoCompraSerializer
 from .models import Usuario, Pago, Vuelo, Asiento, CarritoCompra
+from rest_framework import status
+from django.contrib.auth import authenticate, login, logout
+
+
+
+
+
+
+
+
+
+
+
+
+class SignupView(generics.CreateAPIView):
+    serializer_class = UsuarioSerializer
+
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+
+class LoginUserView(APIView):
+    
+    
+    def post(self, request):
+        # Recuperamos las credenciales y autenticamos al usuario
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+        user = authenticate(email=email, password=password)
+        # Si es correcto añadimos a la request la información de sesión
+        if user:
+            login(request, user)
+            return Response(
+                UsuarioSerializer(user).data,
+                status=status.HTTP_200_OK)
+        # Si no es correcto devolvemos un error en la petición
+        return Response(
+            status=status.HTTP_404_NOT_FOUND)
+
+
+class LogoutUserView(APIView):
+   
+    def post(self, request):
+        # Borramos de la request la información de sesión
+        logout(request)
+
+        # Devolvemos la respuesta al cliente
+        return Response(status=status.HTTP_200_OK)
+class SignupView(generics.CreateAPIView):
+    serializer_class = UsuarioSerializer
+        
+# class LogoutView(APIView):
+    
+#     def post(self, request):
+#         # Borramos de la request la información de sesión
+#         logout(request)
+
+#         # Devolvemos la respuesta al cliente
+#         return Response(status=status.HTTP_200_OK)
+            
+        
+
+
+
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset=Usuario.objects.all()
