7ba532296030e7387cad5545f665140292107baf
RominaRodriguezMoreau <rominarodriguezmoreau@gmail.com>
2023-06-04 17:37:14 -1100

    ultimos arreglos cors, resgistro, autenticacion, preparacion django

--- backend/cba_fly_drf_project/urls.py
+++ backend/cba_fly_drf_project/urls.py
@@ -1,24 +1,21 @@
-"""
-URL configuration for cba_fly_drf_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
 
+from api.views import RegisterUserView, LoginUserView, LogoutUserView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
     
-]
+  
+    path('api/register/', RegisterUserView.as_view(), name='register_user'),
+    path('api/login/', LoginUserView.as_view(), name='login_user'), 
+    path('api/logout/', LogoutUserView.as_view(), name='logout_user'), ]
+   
+
+
+
